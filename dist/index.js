d96baff3e41521e3065ba285ceec9f3019d1cd1c
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-12-08 10:48:38 +0530

    feat: add initial TypeScript files and configuration

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `hey ved this is  ${name}`;
+}
+const username = "Anuuu";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function greet(name :string ) : string {
+    return `hey ved this is  ${name}`;
+}
+const username = "Anuuu";
+console.log(greet(username));
