4a668d9c3a3214eb714a43d771699b01393571fd
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-12-15 17:50:41 +0530

    feat: add interFace.js and interFace.js.map files with initial implementation

---
+++ dist/interFace.js
@@ -0,0 +1,21 @@
+"use strict";
+// #1----------------------------------------
+Object.defineProperty(exports, "__esModule", { value: true });
+class VedantKichen {
+    favlour = "garlic";
+    feelingQuat = 5;
+}
+class momipura {
+    order = "ek full";
+}
+function milfs(girly) {
+    console.log(girly);
+}
+milfs("max");
+const hostel = {
+    chickenpiece: 5,
+    butter: 10
+};
+console.log(hostel.chickenpiece);
+// #5----------------------------------------
+//# sourceMappingURL=interFace.js.map
