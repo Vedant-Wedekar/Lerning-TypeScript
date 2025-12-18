ac5a237b15d1e38911854130e0ef7baf1e2ffdaf
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-12-18 20:30:57 +0530

    feat: add array and enum TypeScript definitions with source map files

---
+++ dist/array,enum.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// ***********************************1****************************************
+const ice_cream = ['choco', 'vanila', 'pista'];
+// console.log(ice_cream[1]);
+// console.log(ice_cream);
+const age = [12, 34, 56, 78];
+const rating = [4.3, 3.4];
+const ice_gola = [
+    { name: 'choce', prices: 10 },
+    { name: 'choce', prices: 10 }
+];
+const uff = [
+    { "name": "vedant", "prices": 10 }
+];
+const city = [
+    [2, 3, 4],
+    [2, 4, 5, 6]
+];
+let bread;
+bread = ["ved", 20];
+const book = ['set', ''];
+// book = ['fddv','']
+var playboy;
+(function (playboy) {
+    playboy[playboy["normy"] = 1] = "normy";
+    playboy[playboy["me"] = 2] = "me";
+    playboy[playboy["africanCurrency"] = 2] = "africanCurrency";
+})(playboy || (playboy = {}));
+//# sourceMappingURL=array,enum.js.map
