d91a9201d1ab9dc434cc9d7f4804c8fe6dccc6c7
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-12-19 20:44:02 +0530

    feat: add Function.js and Function.d.ts.map with initial implementations

---
+++ dist/Function.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// ------------------------------------1----------------------------------------------
+function orderfromstrangerthings(char, powerlevel) {
+    console.log(`hey mf u choose ${char} likely is ur fave character here is her power level ${powerlevel}`);
+}
+// orderfromstrangerthings('EL',17);
+// -----------------------------------------2--------------------------------------
+function zomato(order) {
+    if (!order) {
+        return null;
+    }
+    else
+        console.log(order);
+}
+// zomato('vd')
+// ================================3=======================================================
+function merabacha(type = 'cutie') {
+    console.log(`haa teri hi baat kar raha hu meri bachu ${type}`);
+}
+merabacha('cutie');
+merabacha();
+merabacha('moti');
+// --------------------------------------------4----------------------------------------
+function senorita(song, nickname, type) {
+    console.log(`heyy tujhe kya lagta hai ye song hai ${song} yaa fir kisika nickname ${nickname} yaa shayad dono hi to nahi tume kya lagta hai , ${type}`);
+}
+senorita(true, 'nahi', "song ");
+//# sourceMappingURL=Function.js.map
